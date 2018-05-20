d961f34ef52a115ce2e5c3c55a66cfbed787d034
LAPTOP-55S5M8IC\USER <n.zainita68@gmail.com>
2018-05-20 12:18:28 +0700

    verifikasi berkas

---
+++ application/views/verifikasi_berkas.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h3>Verifikasi Berkas</h3>
+	<tr>
+		<td>
+			<form>
+		Nama : <br>
+		<input type="text" name="Auto"> <br>
+		Id Siswa : <br>
+		<input type="text" name="Auto">
+	</form>
+	<p>
+		Berkas data yang perlu dilengkapi siswa adalah : <br>
+		1. Mengisi Formuliar pendaftaran <br>
+		2. Kertu keluarga <br>
+		3. Ijazah<br>
+		4. Bukti Riwayat Kesehatan<br>
+		5. Pekerjaan Orangtua<br>
+		6. Data Imunisasi Bagi Pendaftar Sekolah TK<br>
+	</p>
+	<br>
+	<br>
+		</td>
+		<td>
+			<button type="button" onclick="alert('Verifikasi Berkas Berhasil!')">Kirim</button>
+		</td>
+	</tr>
+	
+
+</body>
+</html>
