f9a5989020c577f0257ed9ce6cd1da57c6314985
DESKTOP-FAMSFKF\bahrularif <bahrularif007@gmail.com>
2018-06-03 03:04:16 +0700

    masih otw blm lengkap ntr sore kelar kalo ga error

--- application/controllers/superadmin/Petugasdata.php
+++ application/controllers/superadmin/Petugasdata.php
@@ -1,21 +1,50 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Petugasdata extends CI_Controller {
+
+	public $model = NULL;
+
 	public function __construct()
 	{
 		parent::__construct();
-		// $this->load->model('m_petugas');
-		// $this->load->library('from_validation');
-		// $this->load->helper('url','html');
+			//Memuat Model
+		$this->load->model('m_petugas');
+		$this->model = $this->m_petugas;	
+		$this->load->database();
+		//memuat helper url
+		$this->load->helper('url');//sbg redirecrt
 	}
-
-
 		public function index(){
-		$this->load->view('adminlte2/profile/petugas');
+		$this->read();
 	}
-		public function insertptg(){
-		// $this->from_validation->set_rules('');
-		// $this->from_validation->set_rules('');
-		// $this->from_validation->set_rules('');
+		public function create(){
+			 if(isset($_POST['btnSubmit'])) {
+            $this->model->id_admin = $_POST['id_admin'];
+            $this->model->nama_admin = $_POST['nama_admin'];
+            $this->model->no_telp = $_POST['no_telp'];
+            $this->model->status = $_POST['status'];
+            $this->model->username_admin = $_POST['username_admin'];
+            $this->model->password_admin = $_POST['password_admin'];
+            $this->model->petugasinsert();
+            redirect('Petugasdata');
+        }
+        	$this->load->view('adminlte2/profile/petugas', ['model'=>$this->model]);
+        }
+        public function read(){
+        	$rows = $this ->model->read();
+        	//$query = $this->db->query("SELECT * FROM admin ORDER BY id_admin");
+        	$this->load->view('adminlte2/profile/petugas', ['rows'=>$rows]);
+        }
+         public function update($id){
+			 if(isset($_POST['btnSubmit'])) {
+            $this->model->id_admin = $_POST['id_admin'];
+            $this->model->nama_admin = $_POST['nama_admin'];
+            $this->model->no_telp = $_POST['no_telp'];
+            $this->model->status = $_POST['status'];
+            $this->model->username_admin = $_POST['username_admin'];
+            $this->model->password_admin = $_POST['password_admin'];
+            $this->model->petugasupdate();
+            redirect('Petugasdata');
+        }
 	}
 }
--- application/models/m_petugas.php
+++ application/models/m_petugas.php
@@ -2,11 +2,63 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_petugas extends CI_Model{
+ 	public $id_admin;
+    public $nama_admin;
+    public $jenis_kelamin;
+    public $no_telp;
+    public $status;
+    public $username_admin;
+    public $password_admin;
+ 
+    public $labels = [];
 
-  public function petugas($inputpetugas) {
-  		$insertptg = $this->db->insert($inputpetugas);
-  		return $insertptg;
+    public function __construct(){
+    	parent:: __construct();
+    	$this->labels = $this-> _attributeLabels();
+    	$this->load->database();
+    }
+
+  public function petugasinsert() {
+  		$sql = sprintf("INSERT INTO admin VALUES('id','nama','kelamin','tlpon','stats,'username','pass')",
+  			$this->id_admin,
+  			$this->nama_admin,
+  			$this->jenis_kelamin,
+  			$this->no_telp,
+  			$this->status,
+  			$this->username_admin,
+  			$this->password_admin);
+  		$this->db->query($sql);
    }
-   
+    public function petugasupdate() {
+  		$sql = sprintf("UPDATE admin SET nama_admin='nama',jenis_kelamin='kelamin',no_telp='tlpon',status='stats',username_admin='username',password_admin='pass', WHERE id_admin='id'",
+  			$this->id_admin,
+  			$this->nama_admin,
+  			$this->jenis_kelamin,
+  			$this->no_telp,
+  			$this->status,
+  			$this->username_admin,
+  			$this->password_admin);
+  		$this->db->query($sql);
+   }
+    public function petugasdelete() {
+  		$sql = sprintf("DELETE FROM admin WHERE id_admin='id'",$this->id_admin);
+  		$this->db->query($sql);
+   }
+    public function read() {
+        // $sql = "SELECT * FROM admin ORDER BY id_admin";
+        $query = $this->db->get('admin');
+        return $query->result();
+    }
+     private function _attributeLabels() {
+        return [
+            'id_admin'=>'Id Petugas: ',
+            'nama_admin'=>'Nama: ',
+            'jenis_kelamin'=>'Jenis Kelamin:',
+            'no_telp'=>'Nomor Telephone: ',
+            'status'=>'Jabatan: ',
+            'username_admin'=>'Username: ',
+            'password_admin'=>'Password: '
+        ];
+    }
  }
  ?>
