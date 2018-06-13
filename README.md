00d0a9b4764f1e19e145026c67bab7c9f50e7781
DESKTOP-5E18HRJ\Biem <Abiemassyikin@gmail.com>
2018-06-13 12:55:38 +0700

    hapus semua data

--- README.md
+++
@@ -1,2 +0,0 @@
-# JustAdminLteNew
-here u can use this template to input your menu
--- application/views/adminlte2/global/06_footer.php
+++
@@ -1,7 +0,0 @@
-  <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b></b>
-    </div>
-    <strong>Copyright &copy; 2018 <a href="http://jti.polije.ac.id">RED BOX TEAM</a>.</strong> All rights
-    reserved.
-  </footer>
